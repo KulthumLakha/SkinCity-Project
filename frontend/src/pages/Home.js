208c2dde60ab2374b16b858a20e8ab829e931942
KulthumLakha <kulthumlakha@outlook.com>
2023-03-20 22:27:44 -0500

    First commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,116 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <>
+            <section className="home-wrapper-1 py-5">
+                <div className="container.xxl">
+                    <div className="row">
+                        <div className="col-6">
+                            <div className="main-banner position-relative">
+                                <img src="images/main-banner-1.jpg" className="img-fluid rounded-3" alt="main banner"/>
+                                <div className="main-banner-content position-absolute">
+                                    <h4> SUPERCHARGED FOR PROS. </h4>
+                                    <h5> iPad S13+ Pro. </h5>
+                                    <p> $999.00 </p>
+                                    <Link className="button">  BUY NOW </Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-6">
+                            <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                                <div className="small-banner position-relative">
+                                    <img src="images/catbanner-03.jpg" className="img-fluid rounded-3" alt="main banner"/>  
+                                    <div className="small-banner-content position-absolute">                
+                                        <h4> SUPERCHARGED FOR PROS. </h4>
+                                        <h5> iPad S13+ Pro. </h5>
+                                        <p> $999.00 </p>
+                                        <Link className="button">  BUY NOW </Link>
+                                    </div>
+                                </div>
+                                <div className="small-banner position-relative">
+                                    <img src="images/catbanner-01.jpg" className="img-fluid rounded-3" alt="main banner"/>  
+                                    <div className="small-banner-content position-absolute">                
+                                        <h4> SUPERCHARGED FOR PROS. </h4>
+                                        <h5> iPad S13+ Pro. </h5>
+                                        <p> $999.00 </p>
+                                        <Link className="button">  BUY NOW </Link>
+                                    </div>
+                                </div>
+                                <div className="small-banner position-relative">
+                                    <img src="images/catbanner-01.jpg" className="img-fluid rounded-3" alt="main banner"/>  
+                                    <div className="small-banner-content position-absolute">                
+                                        <h4> SUPERCHARGED FOR PROS. </h4>
+                                        <h5> iPad S13+ Pro. </h5>
+                                        <p> $999.00 </p>
+                                        <Link className="button">  BUY NOW </Link>
+                                    </div>
+                                </div>
+                                <div className="small-banner position-relative">
+                                    <img src="images/catbanner-01.jpg" className="img-fluid rounded-3" alt="main banner"/>  
+                                    <div className="small-banner-content position-absolute">                
+                                        <h4> SUPERCHARGED FOR PROS. </h4>
+                                        <h5> iPad S13+ Pro. </h5>
+                                        <p> $999.00 </p>
+                                        <Link className="button">  BUY NOW </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="home-wrapper-2 py-5">
+                <div className="container.xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="services d-flex align-items-center justify-content-between">
+                                <div className="d-flex align-items-center gap-15">
+                                    <img src="images/service.png" alt="services"/>
+                                    <div>
+                                        <h6> Free Shipping </h6> 
+                                        <p className="mb-0"> From all orders over $50 </p>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center gap-15">
+                                    <img src="images/service-02.png" alt="services"/>
+                                    <div>
+                                        <h6> Daily Offers </h6> 
+                                        <p className="mb-0"> Save upto 30% off </p>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center gap-15">
+                                    <img src="images/service-04.png" alt="services"/>
+                                    <div>
+                                        <h6> Affordable Prices </h6> 
+                                        <p className="mb-0"> Wholesale prices </p>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center gap-15">
+                                    <img src="images/service-03.png" alt="services"/>
+                                    <div>
+                                        <h6> 24/7 Support </h6> 
+                                        <p className="mb-0"> Shop with an expert </p>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center gap-15">
+                                    <img src="images/service-05.png" alt="services"/>
+                                    <div>
+                                        <h6> Secure Payment </h6> 
+                                        <p className="mb-0"> 100% Protection guaranteed </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="home-wrapper-3 py-5">
+                /* This section can use swiper for featured products */
+            </section>
+        </>
+    );
+};
+
+export default Home;
