8bca6ed7eb2caa21ff71b39960c365e504a8f3c8
KulthumLakha <kulthumlakha@outlook.com>
2023-03-24 18:24:08 -0500

    Shopping added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
@@ -8,17 +8,20 @@ import Account from "./pages/Account";
 import Cart from "./pages/Cart";
 import Terms from "./pages/Terms";
 
+function App() {
+  const [cartItems, setCartItems] = useState([]);
 
-function App() 
-{
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route
+            index
+            element={<Home setCartItems={setCartItems} />}
+          />
           <Route path="contact" element={<Contact />} />
           <Route path="account" element={<Account />} />
-          <Route path="cart" element={<Cart />} />
+          <Route path="cart" element={<Cart cartItems={cartItems} />} />
           <Route path="terms" element={<Terms />} />
         </Route>
       </Routes>
