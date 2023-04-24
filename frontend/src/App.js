5a0b03343c3c383d3c08cdab5a707ab48c512dca
mykahjw <mykahwhite@my.unt.edu>
2023-04-23 20:20:30 -0500

    sign-in page creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Account from "./pages/Account";
 import Cart from "./pages/Cart";
 import Terms from "./pages/Terms";
+import Container from 'react-bootstrap/Container';
+import SignInPage from './pages/SignInPage';
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
@@ -23,6 +25,7 @@ function App() {
           <Route path="account" element={<Account />} />
           <Route path="cart" element={<Cart cartItems={cartItems} setCartItems={setCartItems} />} />
           <Route path="terms" element={<Terms />} />
+          <Route path="signin" element={<SignInPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
