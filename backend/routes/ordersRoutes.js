4f1530f54e4c6d76cba878a8d0d7f1cd064382be
Suzanne Nolba <suzannenolba@gmail.com>
2023-04-30 01:30:07 -0500

    Order Action

--- backend/routes/ordersRoutes.js
+++ backend/routes/ordersRoutes.js
@@ -1,10 +1,10 @@
-import express from "express";
-import { placeOrder } from "../controllers/ordersControllers.js";
-import { isAuth } from "../utils.js";
+import express from 'express';
+import { placeOrder } from '../controllers/ordersControllers.js';
+import { isAuth } from '../utils.js';
 
 const router = express.Router();
 
-router.post("/", isAuth, placeOrder);
+router.post('/', isAuth, placeOrder);
 // TO-DO:
 // payOrder()
 // getOrderHistory()
