3d5971caaf45d8ad3c42b48c025c7c8b37833e4a
Chalieta <chalietaudreylia@gmail.com>
2023-04-30 02:42:48 -0500

    Fixed placeOrder and added getOrder.

--- backend/routes/ordersRoutes.js
+++ backend/routes/ordersRoutes.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import { placeOrder } from '../controllers/ordersControllers.js';
-import { isAuth } from '../utils.js';
+import express from "express";
+import { placeOrder, getOrder } from "../controllers/ordersControllers.js";
+import { isAuth } from "../utils.js";
 
 const router = express.Router();
 
-router.post('/', isAuth, placeOrder);
+router.post("/", isAuth, placeOrder);
+router.get("/:id", isAuth, getOrder);
 // TO-DO:
 // payOrder()
 // getOrderHistory()
