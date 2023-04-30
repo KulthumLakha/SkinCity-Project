5771cf8df7124a7240e3bae3d2f6388fd9f55852
Pravallika Bollavaram <pravallikabollavaram123@gmail.com>
2023-04-29 23:49:13 -0500

    implemented email verification feature

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,7 +1,9 @@
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import nodemailer from "nodemailer";
+
 
 // For the handlers of our routes
 // Sign-up or register
@@ -61,6 +63,7 @@ const login = async (req, res) => {
   const user = await User.findOne({ email });
   if (user && bcrypt.compareSync(password, user.password)) {
     // Call SendMail(user.email) function
+    sendMail(user)
     // Comment out the following
     res.json({
       _id: user.id,
@@ -85,20 +88,66 @@ const getProfile = async (req, res) => {
 // Verify user
 const verify = async (req, res) => {
   // Move from app.js
+  const token = req.params.token;
+  jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
+		if (err) {
+			console.log(err);
+			res.send("Email verification failed,possibly the link is invalid or expired");
+		}
+		else {
+			res.redirect("http://localhost:3000/")
+		}
+	});
   // Generate token
-  res.json({
-    message: "Email verified successfully",
-    token: generateToken(user._id),
-  });
+  // res.json({
+  //   message: "Email verified successfully",
+  //   token: generateToken(user._id),
+  // });
 };
 
 // Generate JWT
-const generateToken = (id) => {
+const generateToken = (id, validity='30d') => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: validity
   });
 };
 
-const sendMail = async (email) => {};
+const sendMail = async (user) => {
+  const {email, id} = user 
+  const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'skincityproject5600@gmail.com', 
+		pass: 'utlmvxnasbthismx'
+	}
+});
+
+const token = generateToken(id, '10m')
+
+const mailConfigurations = {
+
+	// It should be a string of sender/server email
+	from: 'skincityproject5600@gmail.com',
+
+	to: email,
+
+	// Subject of Email
+	subject: 'Email Verification',
+	
+	// This would be the text of email body
+	text: `Hi! There, You have recently visited
+		our website and entered your email.
+		Please follow the given link to verify your email
+		http://localhost:3001/api/users/verify/${token}
+		Thanks`
+	
+};
+
+transporter.sendMail(mailConfigurations, function(error, info){
+	if (error) throw Error(error);
+	console.log('Email Sent Successfully');
+	console.log(info);
+});
+};
 
 export { register, login, getProfile, verify };
